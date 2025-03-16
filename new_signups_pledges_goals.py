aff5aee9513760952c9b8605cd11793a062ec7b5
jragh <jordanraghunandan@gmail.com>
2025-03-16 01:09:32 -0400

    #1 Corrected and Added KRI Cards for Pledge and Donor performance; Days for 100 signup rate and FY 2025 vs 2024 Performance outstanding

--- new_signups_pledges_goals.py
+++ new_signups_pledges_goals.py
@@ -14,6 +14,7 @@ def generateNewSignupsPledgesGoals(goal_year):
 
 
     ## Pull Initial Data from Polars ##
+    ## Queries ##
     query_new_sign_ups_total = '''
 
     with a as (
@@ -40,35 +41,129 @@ def generateNewSignupsPledgesGoals(goal_year):
 
         )
 
-        select "Fiscal Year",
-        cast(COUNT(distinct pledge_id) as real) as "Number of Pledges"
+        select cast(COUNT(distinct donor_id) as real) as "Number of Donors"
         from a
-        where pledge_status in ('Active donor', 'One-Time')
-        group by "Fiscal Year"
-        order by cast("Fiscal Year" as int) asc 
+        where (pledge_status in ('Active donor', 'One-Time'))
     
     '''
 
-    polars_new_sign_ups_total = pl.read_database_uri(query=query_new_sign_ups_total, uri=postgres_uri, engine='adbc')
+    query_active_pledges_total = '''
 
-    gv1 = polars_new_sign_ups_total.filter(pl.col('Fiscal Year') == goal_year).select(pl.col('Number of Pledges')).item()
+    with a as (
+
+        select case 
+        	when date_part('month', cast(pledge_starts_at as date)) >= 7 then date_part('year', cast(pledge_starts_at as date)) + 1
+        	else date_part('year', cast(pledge_starts_at as date))
+        end as "Fiscal Year",
+
+        case
+        	when date_part('month', cast(pledge_starts_at as date)) >= 7 then date_part('month', cast(pledge_starts_at as date)) - 6
+        	else date_part('month', cast(pledge_starts_at as date)) + 6 
+        end as "Fiscal Month",
+
+        cast(pledge_starts_at as date),
+        pledge_status, 
+        case when pledge_status = 'One-Time' then 'One-Time' else 'Subscription' end as "Pledge Type",
+        pledge_id,
+        donor_id
+
+        from oftw.public.oftw_pledges_raw opr 
+        where pledge_status not in ('ERROR', 'Payment failure')
+        and date_part('year', cast(pledge_starts_at as date)) >= 2015
+
+        )
+
+        select cast(COUNT(distinct pledge_id) as real) as "Number of Pledges"
+        from a
+        where (pledge_status in ('Active donor'))
+    
+    '''
+
+    polars_total_active_donors = pl.read_database_uri(query=query_new_sign_ups_total, uri=postgres_uri, engine='adbc')
+
+    polars_total_active_pledges = pl.read_database_uri(query=query_active_pledges_total, uri=postgres_uri, engine='adbc')
+
+    gv1 = polars_total_active_donors.select(pl.col('Number of Donors')).item()
+    gv1_goal = 1200
+
+    gv1_progress = []
+
+    if round((gv1 / gv1_goal), 2) > 1.00:
+
+        gv1_progress = [
+
+            dmc.ProgressSection(dmc.ProgressLabel(f'100%'), value=round(((100.00 / gv1) * gv1_goal), 0), color='cyan'),
+            dmc.ProgressSection(dmc.ProgressLabel(f'{round(((100.00 / (gv1_goal * 1.00)) * (gv1 - gv1_goal)), 0):.0f}%'), value=(100 - round(((100.00 / gv1) * gv1_goal), 0)), color='green')
+
+        ]
+
+    else:
+
+        gv1_progress = [
+
+            dmc.ProgressSection(dmc.ProgressLabel(f'{round(((100.00 / (gv1_goal*1.00)) * gv1), 0):.0f}%'), value=round(((100.00 / (gv1_goal * 1.00)) * gv1), 0), color='cyan')
+
+        ]
+
+    ## gv2 section ##
+    gv2 = polars_total_active_pledges.select(pl.col('Number of Pledges')).item()
+    gv2_goal = 850
+
+    gv2_progress = []
+
+    if round((gv2 / gv2_goal), 2) > 1.00:
+
+        gv2_progress = [
+
+            dmc.ProgressSection(dmc.ProgressLabel(f'100%'), value=round(((100.00 / gv2) * gv2_goal), 0), color='cyan'),
+            dmc.ProgressSection(dmc.ProgressLabel(f'{round(((100.00 / (gv2_goal * 1.00)) * (gv2 - gv2_goal)), 0):.0f}%'), value=(100 - round(((100.00 / gv2) * gv2_goal), 0)), color='green')
+
+        ]
+
+    else:
+
+        gv2_progress = [
+
+            dmc.ProgressSection(dmc.ProgressLabel(f'{round(((100.00 / (gv2_goal*1.00)) * gv2), 0):.0f}%'), value=round(((100.00 / (gv2_goal * 1.00)) * gv2), 0), color='cyan')
+
+        ]
+
 
     return_array = [
         html.H2('New Sign Ups & Pledges', style={'marginBottom': '0'}),
         html.H4('Key Objectives & Goals', style={'color': 'grey', 'marginTop': '0'}),
         dmc.Group([
             
-            ## Card for new sign ups ##
+            ## Card for Total Active Donors ##
             dmc.Paper([
                 
-                html.H5('New Sign Ups', style={'fontWeight': 'bold', 'marginTop': '0', 'marginBottom': '0.1em'}),
-                html.Small('Active & One Time', style={'fontSize': '65%', 'fontWeight': 'bold', 'color': 'grey'}),
+                html.H5('Total Active Donors', style={'fontWeight': 'bold', 'marginTop': '0', 'marginBottom': '0.1em'}),
+                html.Small('Active & One Time Donors', style={'fontSize': '65%', 'fontWeight': 'bold', 'color': 'grey'}),
+                html.Small(f'Goal: {gv1_goal:.0f}', style={'fontSize': '65%', 'fontWeight': '500', 'color': 'grey', 'fontStyle': 'italic'}),
 
                 html.H2(f'{gv1:,.0f}', style={'marginTop': '0.5em', 'marginBottom': '0'}), 
 
                 dmc.ProgressRoot(
 
-                    dmc.ProgressSection(dmc.ProgressLabel(f'{round(((100.00 / 1200.00) * gv1), 0):.0f}%'), value=round(((100.00 / 1200.00) * gv1), 0), color='cyan'),
+                    gv1_progress,size='xl', mb='xs'
+
+                )
+
+            ], shadow='lg', withBorder=True, radius='lg', px='md', py='xs', className='keys-objs-head-card'),
+
+            ## Card for Total Active Pledges ##
+            ## Distinct Count of Active Donor Pledges ##
+            dmc.Paper([
+                
+                html.H5('Total Active Pledges', style={'fontWeight': 'bold', 'marginTop': '0', 'marginBottom': '0.1em'}),
+                html.Small('Active Pledges Only', style={'fontSize': '65%', 'fontWeight': 'bold', 'color': 'grey'}),
+                html.Small(f'Goal: {gv2_goal:.0f}', style={'fontSize': '65%', 'fontWeight': '500', 'color': 'grey', 'fontStyle': 'italic'}),
+
+                html.H2(f'{gv2:,.0f}', style={'marginTop': '0.5em', 'marginBottom': '0'}), 
+
+                dmc.ProgressRoot(
+
+                    gv2_progress,
                     size='xl', mb='xs'
 
                 )
